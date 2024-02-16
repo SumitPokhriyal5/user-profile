4d421f7f70dc8d720388e849c0765bc9fa8195a1
Sumit Pokhriyal <sumitstan1998@gmail.com>
2024-02-16 16:28:43 +0530

    created achivement section ui for the profile page and fetched rank data

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -2,8 +2,9 @@ import { useEffect, useState } from "react";
 import '../styles/profile.css'
 import AchivementBox from "../components/AchivementBox";
 const Profile = () => {
-    const [profileData, setProfileData] = useState(null);
-    const [pointsAndLevel, setPointsAndLevel] = useState(null);
+    const [profileData, setProfileData] = useState({});
+    const [pointsAndLevel, setPointsAndLevel] = useState({});
+    const [rank, setRank] = useState("");
     const apiCredentials = {
         url: "https://staging.questprotocol.xyz",
         userId: "u-a2399489-9cd0-4c94-ad12-568379202b08",
@@ -19,6 +20,8 @@ const Profile = () => {
         'token': apiCredentials.token,
         'Content-Type': 'application/json'
       }
+
+       
     useEffect(() => {
         const fetchProfileData = async () => {
           try {
@@ -49,11 +52,27 @@ const Profile = () => {
               console.error('Error fetching points and level data:', error);
             }
         };
+
+        const fetchRank = async () => {
+            try {
+              const response = await fetch(`${apiCredentials.url}/api/entities/${apiCredentials.entityId}/users/${apiCredentials.userId}/xp-leaderboard-rank`, {
+                  headers
+              });
+              if (!response.ok) {
+                throw new Error('Failed to fetch points and level data');
+              }
+              const data = await response.json();
+              setRank(data.data.position);
+            } catch (error) {
+              console.error('Error fetching points and level data:', error);
+            }
+        };
     
         fetchProfileData();
         fetchPointsAndLevel();
+        fetchRank();
       }, [])
-    //   console.log('points and level data', pointsAndLevel)
+      console.log('Rank data', rank)
   return (
     <div className="profilePage">
         <h1>Profile</h1>
@@ -63,6 +82,8 @@ const Profile = () => {
             </div>
             <h2>{profileData?.name}</h2>
             <div className="userAchievementContainer">
+                <AchivementBox value={pointsAndLevel.data} title={"Points"} />
+                <AchivementBox value={`#${rank}`} title={"Ranks"} />
                 <AchivementBox value={pointsAndLevel.data} title={"Points"} />
             </div>
         </div>
